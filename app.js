162272685caa10523a5e1f90c6bc436d64a0b074
indra424 <41354551+indra424@users.noreply.github.com>
2018-07-18 20:31:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,98 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const Simsimi = require('simsimi');
-const {
-    token,
-    channelChatbotId,
-    simsimiOption
-} = require('./config.json');
 
-var simsimi = new Simsimi(simsimiOption);
-
-var arrMuteBot = [];
-client.on('ready', function (message) {
-    let today = new Date();
-    let curHr = today.getHours();
-    let greetMsg = "";
-
-    if (curHr < 12) {
-        greetMsg = 'selamat pagi';
-    } else if (curHr < 18) {
-        greetMsg = 'selamat siang';
-    } else {
-        greetMsg = 'selamat malam';
-    }
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-    client.user.setActivity(`HAGO`);
-    //var channel = client.channels.get('469009128749269003');
-    //channel.sendMessage('@everyone บอทมาแล้วจ้าาาาาาา ' + greetMsg + ':pray::skin-tone-3: :pray::skin-tone-3: :pray::skin-tone-3:')
-    //channel.sendMessage('อย่ายุ่งกะกูกูงอล ชิชิชิ' )
-    //channel.sendMessage('บอทไปก่อนน้าาาา บุ้ยบุย...' )
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-function search(key, array, remove) {
-    if (remove) {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] === key) {
-                console.log('remove : ', arrMuteBot[i]);
-                arrMuteBot.splice(i, 1);
-                console.log('array  : ', arrMuteBot);
-                //return array[i];
-                return true;
-            }
-        }
-        return false;
-    } else {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] === key) {
-                //return array[i];
-                return true;
-            }
-        }
-        return false;
-    }
-
-}
-
-client.on('message', function (message) {
-    if (message.author.bot) return;
-    //private message to bot
-    if (message.channel.type === 'dm') {
-        if (message.content.indexOf('!msg') === 0) {
-            let msg = message.content.split(' ');
-            if (msg.length > 1) {
-                var channel = client.channels.get(channelChatbotId);
-                channel.sendMessage(msg[1]);
-            }
-        }
-        return;
-    }
-    if (message.channel.id != channelChatbotId) return;
-
-    if (message.content == '!unmute') {
-        if (search(message.author.id, arrMuteBot, true)) {
-            message.reply('unmute เรียบร้อย คิดถึงเค้าละซิ้');
-        } else {
-            return console.log('user not found : ', message.author.id);
-        }
-
-    }
-
-    //check user mute bot? if true not response that user.
-    if (search(message.author.id, arrMuteBot, false)) return;
-
-    if (message.content == '!mute') {
-        if (search(message.author.id, arrMuteBot, false)) return;
-        arrMuteBot.push(message.author.id);
-        message.reply('ชิชิชิ บังบาจ mute เค้าไปกะได้ *หากต้องการ unmute พิมพ์ !unmute');
-    } else if (message.content != '!mute' && message.content != '!unmute') {
-        simsimi.listen(message.content, function (err, msg) {
-            if (err) return console.error(err);
-            console.log('simsimi say : ', msg)
-            message.reply(msg);
-        });
-    }
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
 });
 
-client.login(token);
+client.login('token');
