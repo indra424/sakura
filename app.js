47703032df451d2cc7a141efb2ed1b6f315fefc0
indra424 <41354551+indra424@users.noreply.github.com>
2018-07-19 20:17:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ client.on('message', function (message) {
         if (search(message.author.id, arrMuteBot, false)) return;
         arrMuteBot.push(message.author.id);
         message.reply('Aku Diam Aja Deh....');
-    } else if (message.content != '!mute' && message.content != '!unmute' && message.content != que[] ) {
+    } else if (message.content != '!mute' && message.content != '!unmute' && message.content != que[0] ) {
         simsimi.listen(message.content, function (err, msg) {
             if (err) return console.error(err);
             console.log('simsimi say : ', msg)
