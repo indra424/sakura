18a32a86b04ae5c7d05d4f3d9c86db8e565badb8
indra424 <41354551+indra424@users.noreply.github.com>
2018-07-20 19:46:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -222,7 +222,7 @@ client.on('message', function (message) {
         if (search(message.author.id, arrMuteBot, false)) return;
         arrMuteBot.push(message.author.id);
         message.reply('Aku Diam Aja Deh....');
-    } else if (message.mentions.id == client.user.id
+    } else if (message.content.startsWith == 'hi'
               && message.content != '!mute' 
               && message.content != '!unmute'
               && message.content != que[0] 
