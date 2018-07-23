3b451e8b589caba73a9dcc4b71569659519741a1
indra424 <41354551+indra424@users.noreply.github.com>
2018-07-23 10:37:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -101,6 +101,12 @@ client.on('ready', function (message) {
     //channel.sendMessage('Jangan Main-Main' )
     //channel.sendMessage('Aku Tidak Tahu Apa Yg Harus Kulakukan..' )
 });
+client.on('message'),function (message) {
+    if (jam == '3')
+    var pesan = client.channels.get(channelChatbotId);
+    pesan.sendMessage('selamat siang hijra');
+    }
+return;
 
 function search(key, array, remove) {
     if (remove) {
