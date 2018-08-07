85ddd7d79cb439b214e2a60934d09f319f558c19
indra424 <41354551+indra424@users.noreply.github.com>
2018-08-07 12:19:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -96,10 +96,6 @@ client.on('message', function (message) {
     }
     //check user mute bot? if true not response that user.
     if (search(message.author.id, arrMuteBot, false)) return;
-//respon pilih
-    if (message.content.startsWith == '!pilih')
-        var cpesan = message.content.split("!pilih").split("atau");
-        message.reply(cpesan[1]);
     if (message.content == '!mute') {
         if (search(message.author.id, arrMuteBot, false)) return;
         arrMuteBot.push(message.author.id);
